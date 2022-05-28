bdc8376198cbd2456e4d2860adca65ec6b5f5c92
testomcode <you@example.com>
2022-05-28 17:08:01 +0200

    Use generics in Dtos

--- src/main/java/se/trafik/antalstop/repository/DataRepository.java
+++ src/main/java/se/trafik/antalstop/repository/DataRepository.java
@@ -2,10 +2,12 @@ package se.trafik.antalstop.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
-import se.trafik.antalstop.dto.JourneyPatternPointOnLineModelDto;
-import se.trafik.antalstop.dto.StopPointModelDto;
+import se.trafik.antalstop.dto.JourneyPatternPointOnLineDto;
+import se.trafik.antalstop.dto.ModelDto;
+import se.trafik.antalstop.dto.StopPointDto;
 
 import javax.annotation.PostConstruct;
 
@@ -15,9 +17,9 @@ public class DataRepository {
     @Value("${api.key}")
     private String apiKey;
 
-    private static JourneyPatternPointOnLineModelDto journeyPatternPointOnLineModelDto = null;
+    private static ModelDto<JourneyPatternPointOnLineDto> journeyPatternPointOnLineModelDto = null;
 
-    private static StopPointModelDto stopPointModelDto = null;
+    private static ModelDto<StopPointDto> stopPointModelDto = null;
 
     @Autowired
     private WebClient webClient;
@@ -28,22 +30,24 @@ public class DataRepository {
                 .get()
                 .uri("/LineData.json?model=jour&key=" + apiKey + "&DefaultTransportModeCode=BUS")
                 .retrieve()
-                .bodyToMono(JourneyPatternPointOnLineModelDto.class)
+                .bodyToMono(new ParameterizedTypeReference<ModelDto<JourneyPatternPointOnLineDto>>() {
+                })
                 .block();
 
         stopPointModelDto = webClient
                 .get()
                 .uri("/LineData.json?model=stop&key=" + apiKey + "&DefaultTransportModeCode=BUS")
                 .retrieve()
-                .bodyToMono(StopPointModelDto.class)
+                .bodyToMono(new ParameterizedTypeReference<ModelDto<StopPointDto>>() {
+                })
                 .block();
     }
 
-    public JourneyPatternPointOnLineModelDto getJourneyPatternPointOnLineModelDto() {
+    public ModelDto<JourneyPatternPointOnLineDto> getJourneyPatternPointOnLineModelDto() {
         return journeyPatternPointOnLineModelDto;
     }
 
-    public StopPointModelDto getStopPointModelDto() {
+    public ModelDto<StopPointDto> getStopPointModelDto() {
         return stopPointModelDto;
     }
 
